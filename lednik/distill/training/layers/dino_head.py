bae102a28ad05732c7855ec9ed94f2fa09ae1bea
pomelk1n <emelya.kara@yandex.ru>
2025-12-22 16:18:37 +0500

    add norm

--- lednik/distill/training/layers/dino_head.py
+++ lednik/distill/training/layers/dino_head.py
@@ -14,7 +14,10 @@ def _build_mlp(
     if nlayers == 1:
         return nn.Linear(in_dim, bottleneck_dim, bias=bias)
     else:
-        layers: list[nn.Module] = [nn.Linear(in_dim, hidden_dim, bias=bias)]
+        layers: list[nn.Module] = [
+            nn.RMSNorm(in_dim),
+            nn.Linear(in_dim, hidden_dim, bias=bias),
+        ]
         if use_bn:
             layers.append(nn.BatchNorm1d(hidden_dim))
         layers.append(nn.GELU())
